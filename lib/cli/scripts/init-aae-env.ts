fb33559b08fcd8e650ea05f5085cdaabf7155be6
Maurizio Vitale <maurizio.vitale@alfresco.com>
2020-12-15 16:07:18 +0000

    Remove useless properties

--- lib/cli/scripts/init-aae-env.ts
+++ lib/cli/scripts/init-aae-env.ts
@@ -39,7 +39,6 @@ export interface ConfigArgs {
     devopsPassword: string;
     clientId: string;
     host: string;
-    oauth: string;
     identityHost: boolean;
 }
 
@@ -80,7 +79,7 @@ async function getApplicationByStatus(status: string) {
         await alfrescoJsApiDevops.login(args.devopsUsername, args.devopsPassword);
 
         return alfrescoJsApiDevops.oauth2Auth.callCustomApi(url, 'GET', pathParams, queryParams, headerParams, formParams, bodyParam,
-            contentTypes, accepts).on('error',(error)=>{
+            contentTypes, accepts).on('error', (error) => {
             logger.error(`Get application by status ${error} `);
         });
 
@@ -207,11 +206,10 @@ function deploy(model: any) {
 function getAlfrescoJsApiInstance(configArgs: ConfigArgs) {
     const config = {
         provider: 'BPM',
-        hostEcm: `${configArgs.host}`,
         hostBpm: `${configArgs.host}`,
         authType: 'OAUTH',
         oauth2: {
-            host: `${configArgs.oauth}/auth/realms/alfresco`,
+            host: `${configArgs.host}/auth/realms/alfresco`,
             clientId: `${configArgs.clientId}`,
             scope: 'openid',
             secret: '',
@@ -383,10 +381,8 @@ async function main(configArgs: ConfigArgs) {
     program
         .version('0.1.0')
         .description('The following command is in charge of Initializing the activiti cloud env with the default apps' +
-            'adf-cli init-aae-env --host "gateway_env"  --oauth "identity_env" --modelerUsername "modelerusername" --modelerPassword "modelerpassword" --devopsUsername "devevopsusername" --devopsPassword "devopspassword"')
+            'adf-cli init-aae-env --host "gateway_env" --modelerUsername "modelerusername" --modelerPassword "modelerpassword" --devopsUsername "devevopsusername" --devopsPassword "devopspassword"')
         .option('-h, --host [type]', 'Host gateway')
-        .option('-o, --oauth [type]', 'Host sso server')
-        .option('-jsonAppsPath, --oauth [type]', 'Host sso server')
         .option('--clientId [type]', 'sso client')
         .option('--modelerUsername [type]', 'username of a user with role ACTIVIT_MODELER')
         .option('--modelerPassword [type]', 'modeler password')
